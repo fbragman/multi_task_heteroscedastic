038af1d9e40283af72f4321e6e0a30cac8729cb5
fbragman <f.bragman@ucl.ac.uk>
2018-01-30 13:15:08 +0000

    changing loss file - should only have the multi-task options individual losses are loaded from appropritate loss modules during the network creations

--- niftynet/layer/loss_multitask.py
+++ niftynet/layer/loss_multitask.py
@@ -22,7 +22,7 @@ class LossFunction(Layer):
         # set loss function and function-specific additional params.
         self._data_loss_func = LossMultiTaskFactory.create(loss_type)
         self._loss_func_params = \
-            loss_func_params if loss_func_params is not None else {}
+            loss_func_params if loss_func_params is not None else dict()
 
     def layer_op(self,
                  prediction,
@@ -75,116 +75,6 @@ class LossFunction(Layer):
                     loss_batch.append(self._data_loss_func(**loss_params))
                 data_loss.append(tf.reduce_mean(loss_batch))
             return tf.reduce_mean(data_loss)
-
-
-def l1_loss(prediction, ground_truth, weight_map=None):
-    """
-    :param prediction: the current prediction of the ground truth.
-    :param ground_truth: the measurement you are approximating with regression.
-    :return: mean of the l1 loss across all voxels.
-    """
-    absolute_residuals = tf.abs(tf.subtract(prediction, ground_truth))
-    if weight_map is not None:
-        absolute_residuals = tf.multiply(absolute_residuals, weight_map)
-        sum_residuals = tf.reduce_sum(absolute_residuals)
-        sum_weights = tf.reduce_sum(weight_map)
-    else:
-        sum_residuals = tf.reduce_sum(absolute_residuals)
-        sum_weights = tf.size(absolute_residuals)
-    return tf.truediv(tf.cast(sum_residuals, dtype=tf.float32),
-                      tf.cast(sum_weights, dtype=tf.float32))
-
-
-def l2_loss(prediction, ground_truth, weight_map=None):
-    """
-    :param prediction: the current prediction of the ground truth.
-    :param ground_truth: the measurement you are approximating with regression.
-    :return: sum(differences squared) / 2 - Note, no square root
-    """
-
-    residuals = tf.subtract(prediction, ground_truth)
-    if weight_map is not None:
-        residuals = \
-            tf.multiply(residuals, weight_map) / tf.reduce_sum(weight_map)
-    return tf.nn.l2_loss(residuals)
-
-
-def rmse_loss(prediction, ground_truth, weight_map=None):
-    """
-    :param prediction: the current prediction of the ground truth.
-    :param ground_truth: the measurement you are approximating with regression.
-    :param weight_map: a weight map for the cost function. .
-    :return: sqrt(mean(differences squared))
-    """
-    if weight_map is not None:
-        residuals = tf.subtract(prediction, ground_truth)
-        residuals = tf.multiply(residuals, residuals)
-        residuals = tf.multiply(residuals, weight_map)
-        return tf.sqrt(tf.reduce_mean(residuals) / tf.reduce_mean(weight_map))
-    else:
-        return tf.sqrt(tf.losses.mean_squared_error(prediction, ground_truth))
-
-
-def mae_loss(prediction, ground_truth, weight_map=None):
-    """
-    :param prediction: the current prediction of the ground truth.
-    :param ground_truth: the measurement you are approximating with regression.
-    :param weight_map: a weight map for the cost function. .
-    :return: mean(abs(ground_truth-prediction))
-    """
-    if weight_map is not None:
-        residuals = tf.subtract(prediction, ground_truth)
-        residuals = tf.abs(residuals)
-        residuals = tf.multiply(residuals, weight_map)
-        return tf.reduce_mean(residuals) / tf.reduce_mean(weight_map)
-    else:
-        return tf.reduce_mean(tf.abs(tf.subtract(prediction, ground_truth)))
-
-
-def huber_loss(prediction, ground_truth, delta=1.0, weight_map=None):
-    """
-    The Huber loss is a smooth piecewise loss function
-    that is quadratic for ``|x| <= delta``, and linear for ``|x|> delta``
-    See https://en.wikipedia.org/wiki/Huber_loss .
-
-    :param prediction: the current prediction of the ground truth.
-    :param ground_truth: the measurement you are approximating with regression.
-    :param delta: the point at which quadratic->linear transition happens.
-    :return:
-    """
-    absolute_residuals = tf.abs(tf.subtract(prediction, ground_truth))
-    residual_is_outside_delta = tf.less(delta, absolute_residuals)
-    quadratic_residual = 0.5 * absolute_residuals ** 2
-    linear_residual = delta * (absolute_residuals - delta / 2)
-
-    voxelwise_loss = tf.where(residual_is_outside_delta,
-                              linear_residual,
-                              quadratic_residual)
-    if weight_map is not None:
-        voxelwise_loss = tf.multiply(voxelwise_loss, weight_map)
-        sum_weights = tf.reduce_sum(weight_map)
-    else:
-        sum_weights = tf.to_float(tf.size(absolute_residuals))
-    sum_loss = tf.reduce_sum(voxelwise_loss)
-    return tf.truediv(sum_loss, sum_weights)
-
-
-def cross_entropy(prediction, ground_truth, weight_map=None):
-    """
-    Function to calculate the cross-entropy loss function
-
-    :param prediction: the logits (before softmax)
-    :param ground_truth: the segmentation ground truth
-    :param weight_map:
-    :return: the cross-entropy loss
-    """
-    entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=prediction, labels=ground_truth)
-    if weight_map is not None:
-        weight_map = tf.cast(tf.size(entropy), dtype=tf.float32) / \
-                     tf.reduce_sum(weight_map) * weight_map
-        entropy = tf.multiply(entropy, weight_map)
-    return tf.reduce_mean(entropy)
 
 
 def homoscedatic_loss_approx(loss_task_1, loss_task_2, sigma_1, sigma_2):
