8ab8e8fe970f2551ed721d256bafcc1ad3f137c8
fbragman <f.bragman@ucl.ac.uk>
2018-01-30 13:44:05 +0000

    refactored loss_multitask

--- niftynet/layer/loss_multitask.py
+++ niftynet/layer/loss_multitask.py
@@ -18,63 +18,19 @@ class LossFunction(Layer):
                  name='loss_function'):
 
         super(LossFunction, self).__init__(name=name)
+        if loss_func_params is not None:
+            self._loss_func_params = loss_func_params
+        else:
+            self._loss_func_params = {}
+        self._data_loss_func = None
+        self.make_callable_loss_func(loss_type)
 
-        # set loss function and function-specific additional params.
-        self._data_loss_func = LossMultiTaskFactory.create(loss_type)
-        self._loss_func_params = \
-            loss_func_params if loss_func_params is not None else dict()
+    def make_callable_loss_func(self, type_str):
+        self._data_loss_func = LossMultiTaskFactory.create(type_str)
 
-    def layer_op(self,
-                 prediction,
-                 ground_truth=None,
-                 weight_map=None):
-        """
-        Compute loss from ``prediction`` and ``ground truth``,
-        the computed loss map are weighted by ``weight_map``.
-
-        if ``prediction`` is list of tensors, each element of the list
-        will be compared against ``ground_truth` and the weighted by
-        ``weight_map``.
-
-        :param prediction: input will be reshaped into
-            ``(batch_size, N_voxels, num_classes)``
-        :param ground_truth: input will be reshaped into
-            ``(batch_size, N_voxels)``
-        :param weight_map: input will be reshaped into
-            ``(batch_size, N_voxels)``
-        :return:
-        """
-
+    def layer_op(self):
         with tf.device('/cpu:0'):
-            batch_size = ground_truth.shape[0].value
-            ground_truth = tf.reshape(ground_truth, [batch_size, -1])
-            if weight_map is not None:
-                weight_map = tf.reshape(weight_map, [batch_size, -1])
-            if not isinstance(prediction, (list, tuple)):
-                prediction = [prediction]
-
-            data_loss = []
-            for ind, pred in enumerate(prediction):
-                # go through each scale
-
-                loss_batch = []
-                for b_ind, pred_b in enumerate(tf.unstack(pred, axis=0)):
-                    # go through each image in a batch
-
-                    pred_b = tf.reshape(pred_b, [-1])
-                    ground_truth_b = ground_truth[b_ind]
-                    weight_b = None if weight_map is None else weight_map[b_ind]
-
-                    loss_params = {
-                        'prediction': pred_b,
-                        'ground_truth': ground_truth_b,
-                        'weight_map': weight_b}
-                    if self._loss_func_params:
-                        loss_params.update(self._loss_func_params)
-
-                    loss_batch.append(self._data_loss_func(**loss_params))
-                data_loss.append(tf.reduce_mean(loss_batch))
-            return tf.reduce_mean(data_loss)
+            return self._data_loss_func(**self._loss_func_params)
 
 
 def homoscedatic_loss_approx(loss_task_1, loss_task_2, sigma_1, sigma_2):
