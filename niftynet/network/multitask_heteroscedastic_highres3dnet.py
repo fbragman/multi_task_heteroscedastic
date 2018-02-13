37b59d13f0af7627f04eefe6c58559b5816d3e78
fbragman <f.bragman@ucl.ac.uk>
2018-02-13 15:56:39 +0000

    fixed num_classes bug, was doing num_classes[0] for both tasks

--- niftynet/network/multitask_heteroscedastic_highres3dnet.py
+++ niftynet/network/multitask_heteroscedastic_highres3dnet.py
@@ -76,7 +76,7 @@ class MTHeteroHighRes3DNet(BaseNet):
             {'name': 'task_1_mean_fc_out', 'n_features': num_classes[0], 'kernel_size': 1},
             {'name': 'task_1_noise_fc_out', 'n_features': 1, 'kernel_size': 1},
 
-            {'name': 'task_1_mean_fc_out', 'n_features': num_classes[0], 'kernel_size': 1},
+            {'name': 'task_1_mean_fc_out', 'n_features': num_classes[1], 'kernel_size': 1},
             {'name': 'task_2_mean_fc_out', 'n_features': 1, 'kernel_size': 1}]
 
     def layer_op(self, images, is_training, layer_id=-1):
