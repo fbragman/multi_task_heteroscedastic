added small constants to make it more numerically stable - not sure if this is correct

niftynet/layer/loss_hetero_regression.py
@@ -110,8 +110,8 @@ def l1_loss(prediction, ground_truth, noise, weight_map=None):
     loss = tf.multiply(absolute_residuals, precision)
     loss = tf.add(loss, noise)
 
-    return tf.truediv(tf.cast(loss, dtype=tf.float32),
-                      tf.cast(sum_weights, dtype=tf.float32))
+    return tf.reduce_mean(tf.truediv(tf.cast(loss, dtype=tf.float32),
+                          tf.cast(sum_weights, dtype=tf.float32)))
 
 
 def l2_loss(prediction, ground_truth, noise, weight_map=None):
@@ -128,7 +128,8 @@ def l2_loss(prediction, ground_truth, noise, weight_map=None):
     # Equation (8) is: (1/2)*exp(-s)||y - pred||^2 + (1/2)
     # where s := log(sigma^2) so sigma = sqrt(exp(s))
 
-    precision = 0.5*tf.exp(-noise)
+    small_constant = 1e-06
+    precision = 0.5*(tf.exp(-noise) + small_constant)
     residuals = tf.subtract(prediction, ground_truth)
 
     if weight_map is not None:

niftynet/layer/loss_hetero_segmentation.py
@@ -14,7 +14,7 @@ from niftynet.layer.base_layer import Layer
 class LossFunction(Layer):
     def __init__(self,
                  n_class,
-                 loss_type='stoch_cross_entropy',
+                 loss_type='scaled_approx_softmax',
                  loss_func_params=None,
                  name='loss_function'):
 
@@ -162,8 +162,9 @@ def scaled_approx_softmax(prediction, ground_truth, noise, T, num_classes):
     loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
         logits=prediction, labels=ground_truth)
 
-    precision = 2*tf.exp(noise)
-    scaled_loss = tf.add(tf.divide(loss, precision), noise)
+    small_constant = 1e-06
+    precision = 0.5*(tf.exp(-noise) + small_constant)
+    scaled_loss = tf.add(tf.multiply(precision, loss), noise)
 
     return tf.reduce_mean(scaled_loss)
 
@@ -194,8 +195,11 @@ def scaled_approx_softmax_img(prediction, ground_truth, noise, T, num_classes):
     loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
         logits=prediction, labels=ground_truth)
 
-    precision = 2*tf.exp(noise)
-    scaled_loss = tf.add(tf.divide(loss, precision), noise)
+
+    small_constant = 1e-05
+
+    precision = 2*tf.exp(noise + small_constant)
+    scaled_loss = tf.add(tf.div(loss, precision), noise)
 
     return scaled_loss
 